7c65816dbbabc469eac9b78b290504a41d35136a
Sebastian <sebastian.gilhofer@gmail.com>
2020-01-12 14:44:42 +0100

    myPhotoAssistantApp

---
+++ myPhotoAssistent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myPhotoAssistent
+//
+//  Created by Sebastian Gilhofer on 12.01.20.
+//  Copyright © 2020 Sebastian Gilhofer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
